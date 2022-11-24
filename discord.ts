3bb8e9ba8b51bc44b5e830178c690940ecb28727
brightwinglover <clayg.gould@gmail.com>
2022-11-24 12:07:41 -0500

    Hotfixing

--- discord.ts
+++ discord.ts
@@ -6,31 +6,31 @@ import {
 import nacl from "https://cdn.skypack.dev/tweetnacl@v1.0.3?dts";
 
 export async function discord(request: Request) {
-  const d = await request.json();
+  // Grab data from request
+  const data = await request.json();
   const { error } = await validateRequest(request, {
     POST: {
       headers: ["X-Signature-Ed25519", "X-Signature-Timestamp"],
     },
   });
-  const valid = await verifySignature(request, d);
+  const valid = await verifySignature(request, data);
   if (error || !valid) {
     return json({ error: "Invalid request." }, { status: 401 });
   }
   // if (!valid) {
   //   return json({ error: "Invalid request" }, { status: 401 });
   // }
-  const { type = 0, data = { options: [] } } = d;
-  console.log("d", d);
-  console.log("Type", type);
-  console.log("Data", data);
+  const { type = 0, input = { data } } = data;
+  // const input = data?.data;
+  // const type = data?.type;
   // Ping
   if (type === 1) return json({ type: 1 });
 
   // Slash commands
   if (type === 2) {
+    // Determine which command by name
     // const { value } = data.options.find((option) => option.name === "name");
-    // switch (data.data.name) {
-    switch (data.name) {
+    switch (input.name) {
       case "hello":
         return json({ type: 4, data: { content: "Hello!" } });
       case "cutie": {
@@ -43,34 +43,26 @@ export async function discord(request: Request) {
   return json({ error: "Bad request." }, { status: 400 });
 }
 
-/** Verify whether the request is coming from Discord. */
 function verifySignature(
   request: Request,
   data: JSON,
 ): boolean {
   const PUBLIC_KEY = Deno.env.get("DISCORD_PUBLIC_KEY")!;
-  // Discord sends these headers with every request.
   const signature = request.headers.get("X-Signature-Ed25519")!;
   const timestamp = request.headers.get("X-Signature-Timestamp")!;
-  // let intermediary = await request.json();
-  // console.log("Intermediary:", intermediary);
-  // const body = JSON.stringify(intermediary);
-  // console.info("Initial body:", body);
-  const body = data;
   const valid = nacl.sign.detached.verify(
     new TextEncoder().encode(timestamp + JSON.stringify(data)),
-    // new TextEncoder().encode(timestamp + JSON.stringify(data)),
     hexToUint8Array(signature),
     hexToUint8Array(PUBLIC_KEY),
   );
   return valid;
 }
 
-/** Converts a hexadecimal string to Uint8Array. */
 function hexToUint8Array(hex: string) {
   return new Uint8Array(hex.match(/.{1,2}/g)!.map((val) => parseInt(val, 16)));
 }
 
+// TODO: Generate cutie dynamically
 function getCutie(): string {
   const url =
     "https://safebooru.org//images/4020/6b6a59a1b454b7571bc9a570becb37acd6c7758d.png?4200107";
