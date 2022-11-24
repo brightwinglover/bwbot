9a58126320eb23a601d82e7c36bc2f1d9d18d1c2
brightwinglover <clayg.gould@gmail.com>
2022-11-23 23:26:24 -0500

    Hotfixing

--- discord.ts
+++ discord.ts
@@ -55,14 +55,13 @@ function verifySignature(
   // Discord sends these headers with every request.
   const signature = request.headers.get("X-Signature-Ed25519")!;
   const timestamp = request.headers.get("X-Signature-Timestamp")!;
-  console.log(PUBLIC_KEY, signature, timestamp);
+  // const body = await request.text();
   const valid = nacl.sign.detached.verify(
     new TextEncoder().encode(timestamp + body),
     hexToUint8Array(signature),
     hexToUint8Array(PUBLIC_KEY),
-    // Buffer.from(signature, "hex"),
-    // Buffer.from(PUBLIC_KEY, "hex"),
   );
+
   return valid;
 }
 
